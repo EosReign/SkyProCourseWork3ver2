2670adf23f2921b520c83dfe343ed3c1729dbf99
EosReign <vsevolod_sol@mail.ru>
2022-11-18 22:08:08 +0300

    Initial commit

---
+++ src/main/java/prosky/skyprocoursework3ver2/SkyProCourseWork3ver2Application.java
@@ -0,0 +1,13 @@
+package prosky.skyprocoursework3ver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProCourseWork3ver2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProCourseWork3ver2Application.class, args);
+    }
+
+}
